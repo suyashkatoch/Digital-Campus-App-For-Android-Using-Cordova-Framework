1344b3205136ec64f2a0da6c040336220e7e60d8
Suyash Katoch <suyashkatoch94@gmail.com>
2016-07-02 11:56:04 +0530

    Update db.php

--- Server Files/db.php
+++ Server Files/db.php
@@ -1,7 +1,6 @@
 <?php
-
-
+#database_name is the name of the database used for storing information.
 $con = mysql_connect("localhost","root","");
-mysql_select_db("hmsit_app",$con);
+mysql_select_db("database_name",$con);
 
 ?>
